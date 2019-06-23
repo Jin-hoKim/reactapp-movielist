82f1e219c7984b906be13fca8394bd393f7687d7
jin-hoKim <wltn222@gmail.com>
2019-06-24 08:23:44 +0900

    Updates

---
+++ precache-manifest.912ed5f37f3d82600c77c291f49f2809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6fc8072fe1a9b7d879724130c5ac4b",
+    "url": "/reactapp-movielist/index.html"
+  },
+  {
+    "revision": "cb55c3126ec4e8e3d7d2",
+    "url": "/reactapp-movielist/static/css/main.97dea922.chunk.css"
+  },
+  {
+    "revision": "883f7a5f8344b1ae06a9",
+    "url": "/reactapp-movielist/static/js/2.b295fdd4.chunk.js"
+  },
+  {
+    "revision": "cb55c3126ec4e8e3d7d2",
+    "url": "/reactapp-movielist/static/js/main.404feaad.chunk.js"
+  },
+  {
+    "revision": "23abc7f9d9b9ba89d8e9",
+    "url": "/reactapp-movielist/static/js/runtime~main.9b585acb.js"
+  }
+]);
